205801b23943f98089c4b9b16a5dd044ebe456d0
Jose Ruiz <jose.ruiz@codeandmodules.com>
2019-04-27 18:53:06 -0700

    removed useless locale

--- scripts/quick-conf.sh
+++ scripts/quick-conf.sh
@@ -12,7 +12,6 @@ timedatectl set-ntp true
 hwclock --systohc
 timedatectl set-timezone America/Los_Angeles
 sed -i 's/^# *\(en_US.UTF-8\)/\1/' /etc/locale.gen
-sed -i 's/^# *\(en_US ISO-8859-1\)/\1/' /etc/locale.gen
 locale-gen
 localectl set-locale LANG=en_US.UTF-8
 
